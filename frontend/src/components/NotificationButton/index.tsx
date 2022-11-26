4b1ec9fcc7d4b005d21814dac544735cb0c1623b
Jâncy Aragão <jancy.aragao@gmail.com>
2022-11-25 23:49:15 -0300

    Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,11 +1,25 @@
+import axios from 'axios';
+
 import './styles.css';
 import icon from '../../assets/img/notification-icon.svg';
+import { BASE_URL } from '../../utils/request';
 
-function NotificationButton() {
+type Props = {
+    saleId: number;
+}
+
+function handleClick(saleId: number) {
+    axios(`${BASE_URL}/${saleId}/notification`).then(response => {
+        console.log("Sucesso, meu chapa!");
+    })
+}
+
+// http://localhost:8080/sales/ID/notification
+function NotificationButton({saleId} : Props) {
 
     return (
         <>
-            <div className="dsmeta-red-btn">
+            <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
                 <img src={icon} alt="Notificar" />
             </div>
         </>
